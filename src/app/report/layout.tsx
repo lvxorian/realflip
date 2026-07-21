2c635e876e91c8f3ab5c0d752371dd3ea82c661b
lvxorian <lvcasdope@gmail.com>
2026-07-21 16:39:45 +0200

    fix: report layout - odstraneno vlastni html/body (prepinani do svetleho rezimu)

--- src/app/report/layout.tsx
+++ src/app/report/layout.tsx
@@ -1,10 +1,5 @@
 import type { ReactNode } from "react";
-import "../globals.css";
 
 export default function ReportLayout({ children }: { children: ReactNode }) {
-  return (
-    <html>
-      <body className="bg-white text-gray-900 p-8 print:p-0">{children}</body>
-    </html>
-  );
+  return <div className="p-8 print:p-0">{children}</div>;
 }
